8419dcd58dc26a5b4561adb3b19888c316fd6856
Emanuel Boaventura <emanuelbmatos321@gmail.com>
2023-09-03 23:03:54 -0400

    feat: install dnd lib and make it works

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,5 @@
+import { Draggable, DragDropContext, Droppable } from "react-beautiful-dnd";
+
 export default function Home() {
   const players = [
     {
@@ -18,6 +20,10 @@ export default function Home() {
     },
   ];
 
+  const handleDragEnd = (result: any) => {
+    // Handle the end of a drag event here
+  };
+
   return (
     <main className="flex flex-col gap-4 items-center justify-between p-10">
       <h1 className="text-2xl font-bold text-center">Bem vindo ao Bozó WEB!</h1>
@@ -26,14 +32,38 @@ export default function Home() {
         Escolha quantos jogadores participarão do jogo.
       </p>
 
-      <div className="grid grid-cols-2 w-full gap-4">
-        {players.map((player) => (
-          <div key={player.name} className="text-center">
-            <p>{player.name}</p>
-            <div className={`${player.color} w-full h-2`} />
-          </div>
-        ))}
-      </div>
+      <DragDropContext onDragEnd={handleDragEnd}>
+        <Droppable droppableId="teste">
+          {(provided) => (
+            <div
+              className="flex flex-col w-full"
+              ref={provided.innerRef}
+              {...provided.droppableProps}
+            >
+              {players.map((player, index) => (
+                <Draggable
+                  key={player.name}
+                  draggableId={player.name}
+                  index={index}
+                >
+                  {(providedTwo) => (
+                    <div
+                      className="text-center border border-black mt-4"
+                      ref={providedTwo.innerRef}
+                      {...providedTwo.draggableProps}
+                      {...providedTwo.dragHandleProps}
+                    >
+                      <p className="mt-2 mb-1">{player.name}</p>
+                      <div className={`${player.color} w-full h-2`} />
+                    </div>
+                  )}
+                </Draggable>
+              ))}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
     </main>
   );
 }
