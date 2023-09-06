9007d743912fb1b0bf22c309ebeb33867ad5850f
Emanuel Boaventura <emanuelbmatos321@gmail.com>
2023-09-05 22:02:51 -0400

    feat: reorder array on drag and drop

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,13 @@
+import { useState } from "react";
 import { Draggable, DragDropContext, Droppable } from "react-beautiful-dnd";
 
+interface IPlayers {
+  name: string;
+  color: string;
+}
+
 export default function Home() {
-  const players = [
+  const [players, setPlayers] = useState<IPlayers[]>([
     {
       name: "Marcos",
       color: "bg-[#ff0000]",
@@ -18,10 +24,29 @@ export default function Home() {
       name: "João Lucas",
       color: "bg-[#ffa500]",
     },
-  ];
+  ]);
+  console.log("players:", players);
 
   const handleDragEnd = (result: any) => {
-    // Handle the end of a drag event here
+    const { destination, source } = result;
+    if (!destination) {
+      return;
+    }
+
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return;
+    }
+
+    const newPlayers = Array.from(players);
+
+    // Remove the item from the source index and insert the removed item at the destination index
+    const [removed] = newPlayers.splice(source.index, 1);
+    newPlayers.splice(destination.index, 0, removed);
+
+    setPlayers(newPlayers);
   };
 
   return (
@@ -33,7 +58,7 @@ export default function Home() {
       </p>
 
       <DragDropContext onDragEnd={handleDragEnd}>
-        <Droppable droppableId="teste">
+        <Droppable droppableId="column-1">
           {(provided) => (
             <div
               className="flex flex-col w-full"
